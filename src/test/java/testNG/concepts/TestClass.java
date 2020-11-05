1d53d067eff8cb060a676dcc464a539225c676f1
olgaannagel <73318445+olgaannagel@users.noreply.github.com>
2020-11-05 16:37:04 -0500

    Update TestClass.java
    changes in git hub

--- src/test/java/testNG/concepts/TestClass.java
+++ src/test/java/testNG/concepts/TestClass.java
@@ -8,6 +8,9 @@ public class TestClass {
 	@Test
 	public void test() {
 		System.out.println("this class will be pushed to repo");
+		//this is comment from git hub account
+		//these are 
+		//to pullthese chenge we need to fitch 
 		
 	}
 
