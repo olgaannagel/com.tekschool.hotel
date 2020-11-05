679711723743ae781bf5997bc54fbddae4022f71
olgaannagel <olga.annagel@gmail.com>
2020-11-05 16:52:14 -0500

    have been a new class

--- src/test/java/testNG/concepts/TestClass.java
+++ src/test/java/testNG/concepts/TestClass.java
@@ -9,8 +9,8 @@ public class TestClass {
 	public void test() {
 		System.out.println("this class will be pushed to repo");
 		//this is comment from git hub account
-		//these are 
-		//to pullthese chenge we need to fitch 
+		//these are changes made in Git Repo
+		//to pull these changes we need to fitch from upstream or use git pull command 
 		
 	}
 
---
+++ src/test/java/testNG/concepts/TestOlgaGitHub.java
@@ -0,0 +1,22 @@
+package testNG.concepts;
+
+public class TestOlgaGitHub {
+
+	
+	public void String () {
+		
+		int a = 10;
+		
+		int b = 11;
+		
+		if (a > b) {
+			System.out.println(a);
+	    if (a < b) {
+			System.out.println(b);
+				
+			}
+		}
+			
+				
+	}
+}
